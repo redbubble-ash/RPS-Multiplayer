220b99958c007d6fd56aa1332bfb7d990bf68c3b
Jing Xie <xiejing418@gmail.com>
2019-05-09 01:23:09 -0700

    added live chat box

--- assets/js/RPS.js
+++ assets/js/RPS.js
@@ -1,3 +1,10 @@
+
+var chatMessage = "";
+var addMessage = ""
+
+
+
+
 var firebaseConfig = {
     apiKey: "AIzaSyAFXZ20ElXP90YlsRYcZ9OcDD10p8Gdi1A",
     authDomain: "rps-multiplayer-game-e1e01.firebaseapp.com",
@@ -11,6 +18,38 @@ firebase.initializeApp(firebaseConfig);
 // Create a variable to reference the database.
 var database = firebase.database();
 
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function (snap) {
+
+    // If they are connected..
+    if (snap.val()) {
+
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+    }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function (snap) {
+
+    // Display the viewer count in the html.
+    // The number of online users is the number of children in the connections list.
+    $("p1").html("There are " + snap.numChildren() + " players");
+    // if(snap.numChildren()>2){
+    //     alert("Game has reached to player limit, sorry you can't join this time.")
+    // }
+});
 
 
 
@@ -46,3 +85,42 @@ function gameResult(player1Click, player2Click) {
 }
 
 gameResult(player1Click, player2Click);
+
+// var messageRef = database.ref("/message");
+
+// messageRef.on("value", function (snap) {
+
+//     $(".chat").prepend(snap);
+
+// });
+
+
+// live chatting function
+// creating child and push message info to newMessage on the server
+$("#message").on("click", function () {
+    event.preventDefault();
+    console.log("check");
+    chatMessage = $("#leaveMessage").val().trim();
+
+    database.ref("/newMessage").push({
+        message: chatMessage,
+    });
+
+
+})
+
+// getting new message info from server and display on chat box.
+// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+database.ref("/newMessage").orderByChild("dateAdded").limitToLast(1).on("child_added", function (childSnapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(childSnapshot.val().message);
+
+    // creating new <p> message line in the chat box
+    addMessage = $("<p>").html(childSnapshot.val().message);
+    $(".chat").prepend(addMessage);
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
